61c22a47ed17079f8339c2b8ef23d3286f4fe6ca
Edgar Pezoa <pezoa.1@gmail.com>
2023-01-08 23:18:15 -0300

    mygame test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { unityProvider } = useUnityContext({
+		loaderUrl: "/mygame/mygame3.loader.js",
+		dataUrl: "/mygame/mygame3.data",
+		frameworkUrl: "/mygame/mygame3.framework.js",
+		codeUrl: "/mygame/mygame3.wasm",
+	});
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				flexWrap: "wrap",
+			}}
+		>
+			<h1 style={{ width: "100%", textAlign: "center" }}>My ship game</h1>
+			<Unity
+				unityProvider={unityProvider}
+				style={{ width: 480, height: 854 }}
+			/>
+		</div>
+	);
 }
 
 export default App;
